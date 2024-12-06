dd8fe1ba0634cb8157bc0d3ab6cdc76cc3008487
xdev <xdev1206@gmail.com>
2024-12-06 17:27:08 +0800

    chore: Update update_git.sh

--- setup/update_git.sh
+++ setup/update_git.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-git config --local user.name "LittleMiner"
-git config --local user.email "nocent@126.com"
-git config --local credential.helper store
+echo "IFS:$IFS, as delimeter"
+
+read -p "set local/global config (L/G)?: " scope && [[ $scope == [lLgG] ]] || exit 1
+read -p "username, like 'Han Lei': " username
+read -p "email: " email
+
+if [[ $scope == [lL] ]]; then
+    option="--local"
+else
+    option="--global"
+fi
+
+git config $option user.name "$username"
+git config $option user.email "$email"
+
+git config --global credential.helper store
 git config --global core.editor vim
-git config --global --add safe.directory /work/xinxu5/bot-cctrl-service
+git config --global --add safe.directory /workspace
 git config --global http.version HTTP/1.1 # HTTP/2
 git config --global http.sslBackend gnutls
